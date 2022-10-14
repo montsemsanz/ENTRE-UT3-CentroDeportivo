927ea0a93ef56b1f6fb525c628b3e333c221012c
Elias Lahmami <elahmamihdam1@gmail.com>
2022-10-14 10:55:31 +0200

    parte del switch y accesor de clase maximo yoga hecho

--- CentroDeportivo.java
+++ CentroDeportivo.java
@@ -25,26 +25,26 @@
  */
 public class CentroDeportivo
 {
-  private final int HORA_PRIMERA_CLASE = 10;
-  private final int MINUTOS_PRIMERA_CLASE = 30;
-  private final int HORA_ULTIMA_CLASE = 8;
-  private final int MINUTOS_ULTIMA_CLASE = 30;
-  private final int DESCANSO = 10;
-  private final double PRECIO_BASE = 5.0;
-  private final double PRECIO_QUINCE_MINUTOS = 0.40;
-  
-  private final char YOGA = 'Y';
-  private final char PILATES = 'P';
-  private final char SPINNING = 'S';
-  
-  private String nombre;
-  private int yoga;
-  private int pilates;
-  private int spinning;
-  
-  private double totalAcumulado;
-  private int salaMaximoYoga;
-  private int  maximoInscripcionesYoga;
+    private final int HORA_PRIMERA_CLASE = 10;
+    private final int MINUTOS_PRIMERA_CLASE = 30;
+    private final int HORA_ULTIMA_CLASE = 8;
+    private final int MINUTOS_ULTIMA_CLASE = 30;
+    private final int DESCANSO = 10;
+    private final double PRECIO_BASE = 5.0;
+    private final double PRECIO_QUINCE_MINUTOS = 0.40;
+
+    private final char YOGA = 'Y';
+    private final char PILATES = 'P';
+    private final char SPINNING = 'S';
+
+    private String nombre;
+    private int yoga;
+    private int pilates;
+    private int spinning;
+
+    private double totalAcumulado;
+    private int salaMaximoYoga;
+    private int  maximoInscripcionesYoga;
 
     /**
      * Constructor  - 
@@ -52,14 +52,14 @@ public class CentroDeportivo
      * e inicializa el resto de atributos adecuadamente 
      */
     public CentroDeportivo(String queNombre)    {
-       nombre = queNombre;
-       yoga = 0;
-       pilates = 0;
-       spinning = 0;
-       totalAcumulado = 0;
-       salaMaximoYoga = 0;
-       maximoInscripcionesYoga = 0;
-       
+        nombre = queNombre;
+        yoga = 0;
+        pilates = 0;
+        spinning = 0;
+        totalAcumulado = 0;
+        salaMaximoYoga = 0;
+        maximoInscripcionesYoga = 0;
+
     }
 
     /**
@@ -74,7 +74,7 @@ public class CentroDeportivo
      *  Mutador para el nombre del centro deportivo
      */
     public void  setNombre(String queNombre)    {
-       nombre = queNombre;
+        nombre = queNombre;
     }
 
     /**
@@ -86,60 +86,71 @@ public class CentroDeportivo
         return totalAcumulado;
     }
 
-    // /**
-     // *  Este método recibe 5 parámetros: 
-     // *      - sala: el nº de sala donde se hace la actividad
-     // *      - tipo: el tipo de actividad, un carácter 'Y' yoga, 'P' pilates 'S' spinning
-     // *      - horas y minutos : duración de la actividad en nº horas y minutos
-     // *      - inscritos: el nº de personas inscritas en esa actividad en la sala
-     // *      
-     // *      Por ej, tarificarClaseEnSala(4, 'P', 1, 5, 15) significa que en la sala 4 se hace
-     // *      pilates, las clases duran 1 hora y  5 minutos y se han inscrito en esta sala 15 personas
-     // *   
-     // *   A partir de esta información el método debe calcular:
-     // *      - total inscritos por tipo de actividad (independientemente de la sala)
-     // *      - la sala con máximo nº de inscritos en yoga y dicho valor máximo 
-     // *   
-     // *   Utiliza una sentencia switch  para analizar el tipo de actividad
-     // *   
-     // *   También el método calculará:
-     // *   
-     // *      - el precio de la clase en la sala (basándose en su duración). En el ejemplo anterior
-     // *      la clase de pilates duraba 1 hora y 5 minutos . Como el total de minutos
-     // *      de duración es 65 su precio será: 5 + 0,40 *  4 = 6,60 ya que son 4 los períodos completos de
-     // *      15 minutos que hay
-     // *      - nº de veces que la clase se ofertará en la sala (dependerá de su duración. No olvidar que entre clase 
-     // *      y clase siempre hay un descanso)
-     // *      - la hora de finalización de la última clase (hora y minutos) - !!Ver resultados de ejecución!!
-     // *      - el método además irá registrando el importe total que lleva acumulado el centro entre todas
-     // *      las personas inscritas 
-     // *      
-     // *   En pantalla se mostrarán los datos de la sala tal como indican los resultados de ejecución
-     // *              
-     // */
-    // public void tarificarClaseEnSala(int sala, char tipo, int horas, int minutos, int inscritos)    {
-    
-    // }
+    /**
+     *  Este método recibe 5 parámetros: 
+     *      - sala: el nº de sala donde se hace la actividad
+     *      - tipo: el tipo de actividad, un carácter 'Y' yoga, 'P' pilates 'S' spinning
+     *      - horas y minutos : duración de la actividad en nº horas y minutos
+     *      - inscritos: el nº de personas inscritas en esa actividad en la sala
+     *      
+     *      Por ej, tarificarClaseEnSala(4, 'P', 1, 5, 15) significa que en la sala 4 se hace
+     *      pilates, las clases duran 1 hora y  5 minutos y se han inscrito en esta sala 15 personas
+     *   
+     *   A partir de esta información el método debe calcular:
+     *      - total inscritos por tipo de actividad (independientemente de la sala)
+     *      - la sala con máximo nº de inscritos en yoga y dicho valor máximo 
+     *   
+     *   Utiliza una sentencia switch  para analizar el tipo de actividad
+     *   
+     *   También el método calculará:
+     *   
+     *      - el precio de la clase en la sala (basándose en su duración). En el ejemplo anterior
+     *      la clase de pilates duraba 1 hora y 5 minutos . Como el total de minutos
+     *      de duración es 65 su precio será: 5 + 0,40 *  4 = 6,60 ya que son 4 los períodos completos de
+     *      15 minutos que hay
+     *      - nº de veces que la clase se ofertará en la sala (dependerá de su duración. No olvidar que entre clase 
+     *      y clase siempre hay un descanso)
+     *      - la hora de finalización de la última clase (hora y minutos) - !!Ver resultados de ejecución!!
+     *      - el método además irá registrando el importe total que lleva acumulado el centro entre todas
+     *      las personas inscritas 
+     *      
+     *   En pantalla se mostrarán los datos de la sala tal como indican los resultados de ejecución
+     *              
+     */
+    public void tarificarClaseEnSala(int sala, char tipo, int horas, int minutos, int inscritos)    {
+        switch(tipo){
+            case YOGA:
+                yoga += inscritos;
+                if(inscritos > maximoInscripcionesYoga){
+                    salaMaximoYoga = sala;
+                }
+            case PILATES:
+                pilates += inscritos;
+                break;  
+            case SPINNING:
+                spinning += inscritos;
+                break;
+        }
+        double precioClase = PRECIO_BASE + (((horas * 60) + minutos ) / 15) * PRECIO_QUINCE_MINUTOS ;
+        totalAcumulado = inscritos + precioClase;
+    }
 
-    // /**
-     // *  nº sala en la que hay más inscritos en yoga
-     // *   
-     // */
-    // public getSala()   {
-         
-        
-    // }
+
+    /**
+     *  nº sala en la que hay más inscritos en yoga
+     *   
+         */
+    public int getSala()   {
+    return salaMaximoYoga ;
+
+    }
 
     // /**
      // * Devuelve el nombre de la actividad con más inscritos 
      // * independientemente de la sala  (puede haber coincidencias)
      // *  
      // */
-    // public getActividadMaximasInscripciones()    {
-        
-        
-        
-        
+    // public   getActividadMaximasInscripciones()    {
+    
     // }
-
- }
+    }
